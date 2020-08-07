4e097af9b82bc05183b6ee5eb3237216a694969b
Ryan Hinkle <Ryan.hinkle@gmail.com>
2020-08-06 22:46:27 -0400

    Expect the logs to see what is being logged

--- src/controller/__tests__/health-check.test.ts
+++ src/controller/__tests__/health-check.test.ts
@@ -5,7 +5,7 @@ import {HealthCheckController} from "../health-check";
 import TYPES from "../../TYPES";
 import supertest = require("supertest");
 import {ILogger} from "../../common/logger/logger";
-import {ConsoleLogger} from "../../common/logger/console-logger";
+import {ConsoleLogger, NullLogger} from "../../common/logger/console-logger";
 
 test('RouteTest: /health-check', async done => {
     //This is for route tests. Any other test not using supertest wont need this.
@@ -19,5 +19,9 @@ test('RouteTest: /health-check', async done => {
     const res: any = await request.get('/health-check');
     expect(res.body).toStrictEqual({status: 'pass'});
     expect(res.status).toBe(200);
+
+    const logger = childContainer.get<NullLogger>(TYPES.ConsoleLogger);
+    expect(logger.getLastLogMessage()).toBe('Request received.');
+
     done();
 });
