f4272cc8d0545532d4c301c062ed39b190941159
Ryan Hinkle <Ryan.hinkle@gmail.com>
2020-12-19 17:58:42 -0500

    Update to version 14 and fix server boot to echo url on boot

--- src/server.ts
+++ src/server.ts
@@ -5,4 +5,5 @@ const app = new App(container).build();
 
 app.listen(3000, () => {
     console.log('** App is listening on port 3000!');
+    console.log('http://localhost:3000/health-check');
 });
